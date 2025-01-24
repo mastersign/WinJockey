2e6d68cd9c3b64f5fd20b044c56021f5343b5d57
Tobias Kiertscher <dev@mastersign.de>
2025-01-24 09:32:13 +0000

    implemented application wide exception reporting

---
+++ src/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+namespace Mastersign.WinJockey
+{
+    internal static class Program
+    {
+        [STAThread]
+        public static int Main(string[] args)
+        {
+            try
+            {
+                App.Main();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "WinJockey", MessageBoxButton.OK, MessageBoxImage.Error);
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
